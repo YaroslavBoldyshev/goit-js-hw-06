1b572cca5dbf9141ffc6e786c759d1459497c335
YaroslavBoldyshev <110547217+YaroslavBoldyshev@users.noreply.github.com>
2022-11-12 09:48:08 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,21 +4,23 @@ function getRandomHexColor() {
 const input = document.querySelector('[type="number"]');
 const container = document.querySelector('#boxes');
 const controls = document.querySelector('#controls');
-
+let counter = 0;
 function createBoxes(amount) {
   const markupArr = [];
-  for (let i = 0; i < amount; i++){
+  for (let i = counter; i < amount; i++){
     const width = 30 + i * 10;
     markupArr.push(`
     <div style="width: ${width}px; height: ${width}px; background-color: ${getRandomHexColor()}"></div>
     `)
+    counter += 1;
   }
   const stringMarkup = markupArr.join('');
-  container.insertAdjacentHTML('afterbegin', stringMarkup);
+  container.insertAdjacentHTML('beforeend', stringMarkup);
 }
 
 function destroyBoxes() {
   container.innerHTML = '';
+  counter = 0;
 }
  
 controls.addEventListener('click', (e) => {
